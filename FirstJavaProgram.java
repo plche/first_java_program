e916988fca7234086e6caa16f71d7925e8fecd41
plche <che.percy.l@gmail.com>
2022-02-14 12:34:29 -0500

    First Java Program

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Percy");
+        System.out.println("Tengo 47 años de edad");
+        System.out.println("Mi ciudad natal es Trujillo, La Libertad, Peru");
+    }
+}
